dc4c10314efffa7b466198da430c5c69d00599a6
Eric Hilary Smith <erichilarysmith@outlook.com>
2020-10-01 15:23:16 -0500

    Add files via upload

---
+++ CAOS.js
@@ -0,0 +1,224 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// always-run-in-app: true; icon-color: pink;
+// icon-glyph: prescription; share-sheet-inputs: plain-text, url, file-url;
+// Chemical Analysis Operating System
+var freewebsBohrscienceprogram;
+freewebsBohrscienceprogram;
+var pharmacy;
+pharmacy;
+var hospital;
+hospital;
+var patient;
+patient;
+if (patient) {
+  console.log("calories");
+  } else {
+    console.log("carbohydrates");
+    }
+var patientHealthInsurance;
+patientHealthInsurance;
+var patientHealthInsurance = ["medicare", "medicaid", "Hmo"];
+patientHealthInsurance.join(" and ")
+patientHealthInsurance.join(" or ")
+for ( var i = 0; i < patientHealthInsurance.length;  i++) {
+  console.log("Some number of blacks in Chicago utilizing " + patientHealthInsurance[i] + ".");
+  }
+  var Medicare;
+  Medicare;
+var Medicare = ["medicareA", "medicareB", "medicareC", "medicareD"];
+Medicare.join(" and ")
+Medicare.join(" or ")
+var Paleos;
+Paleos;
+var Paleos = ["Mcdonalds", "Walgreens", "KentuckyFriedChicken"];
+Paleos.join(" or ")
+var Paleos = {
+  "schools":  "ChartwellsHigherEducation",
+  "Fastfood":  "McData",
+  "Fastfood1":  "KentuckyFriedChicken"
+  };
+var Paleos = 0;
+while (Paleos < 6) {
+  console.log("Some number of patients can eat " + Paleos + " meals per day.");
+  Paleos++;
+  }
+  console.log("McData Github");
+  var Paleos = 0;
+  while (Paleos < 17) {
+    console.log("Some number of patients may decrease random Glucose levels by drinking diet Coca Cola in amounts of " + Paleos +   " ounces.");
+    Paleos++;
+    }
+    console.log("Drinking Diet Coca Cola");
+    var patientComplications;
+    patientComplications;
+var patientComplications = ["prediabetes", "Type1Diabetes", "Type2Diabetes"];
+for ( var i = 0;  i < patientComplications.length;  i++) {
+  console.log("Some number of blacks in Chicago living with " + patientComplications[i] + ".");
+  }
+  var patientNoComplications;
+  patientNoComplications;
+var patientNoComplications = ["hypertension", "adiposity", "obesity"];
+var BehindCounterDrugs;
+BehindCounterDrugs;
+var BehindCounterDrugs = ["Insulin", "Glucophage", "Metformin"];
+var OverCounterDrugs;
+OverCounterDrugs;
+var OverCounterDrugs = ["Nootropics", "Homeopathics", "WeightManagement", "Vitamins", "Cofactors", "Herbs"];
+var Walgreens;
+Walgreens;
+var Walgreens = ["BehindCounterDrugs", "OverCounterDrugs", "MedicalDevices", "Meals"];
+var devicesMedical;
+devicesMedical;
+var devicesMedical = ["Dexcom", "Sifidious", "ImhotepBizarros", "HysteriaPTrance"];
+var devicesMedicalFunction = function () {
+  console.log("Some number of patients are players.");
+  };
+  devicesMedicalFunction();
+var Dexcom = {
+  "patientComplications":  "Type2Diabetes",
+  "patientGlucoseLevel":  "milligramsperdeciliter",
+  "patientWeight":  "pounds",
+  "patientHeight":  "inches",
+  "BehindCounterDrugs":  "Metformin"
+  };
+var Walgreens = {
+  "hospitalcollaboration":  "AdvocateMedicalGroup",
+  "universitycollaboration":  "UniversityIllinoisSystem",
+  "patientComplications":  "Type2Diabetes",
+  "patientNoComplications":  "Hypertension",
+  "patientPharmacyPrescriptionRewards":  "Insulin6040Medication",
+  "CommunityServiceProvider":  "HeartlandAllianceHealth"
+  };
+  var McDonalds;
+  McDonalds;
+var McDonalds = ["Breakfast", "Lunch", "Dinner"];
+McDonalds.join(" or ")
+McDonalds.join(" and ")
+var Atoms;
+Atoms;
+var Atoms = [];
+var Atoms = ["Carbon", "Hydrogen", "Oxygen", "Nitrogen", "Magnesium"];
+Atoms.join(" or ")
+Atoms.join(" and ")
+for ( var i = 0;  i < Atoms.length;  i++) {
+  console.log("Some number of " + Atoms[i] + ".");
+  }
+  var AtomsFunction = function () {
+    console.log("There exist some number of atoms.");
+    };
+    AtomsFunction();
+    var AtomsFunction = function () {
+      console.log("Let atoms such that atoms is some number of atoms.");
+      };
+      AtomsFunction();
+    for ( var i = 0;  i < Atoms.length;  i++) {
+  console.log("The standard atomic weight of " + Atoms[i] + ".");
+  }
+  var Atoms = "Hydrogen";
+  console.log("Some number of " + Atoms);
+  if (Atoms.length > 5) {
+    console.log("It can bind with 2 Oxygen atoms to make water.");
+    }
+    var Atoms = 0;
+    while (Atoms < 5) {
+      console.log("Carbon has " + Atoms + " electrons per shell.");
+      Atoms++;
+      }
+      console.log("Carbon Atom");
+      var Atoms = 0;
+      while (Atoms < 2) {
+        console.log("Hyrogen has " + Atoms + " electrons per shell.");
+        Atoms++;
+        }
+        console.log("Hydrogen Atom");
+        var Atoms = 0;
+        while (Atoms < 7) {
+          console.log("Oxygen has " + Atoms + " electrons per shell.");
+          Atoms++;
+          }
+          console.log("Oxygen Atom")
+          var Atoms = 0;
+          while (Atoms < 6) {
+            console.log("Nitrogen has " + Atoms + " electrons per shell.");
+            Atoms++;
+            }
+            console.log("Nitrogen Atom")
+            var devicesFitbit;
+            devicesFitbit;
+        var devicesFitbit = ["patientComplications", "patientSleep", "patientCalories", "patientWalkingSpeed"];
+        var devicesFitbit = {
+          "patientSleep":  "Hours",
+          "patientCalories":  2400,
+          "patientWalkingSpeed":  "StepsPerMinute",
+          "patientComplications":  "Diabetes"
+          };
+for ( var i = 0;  i < devicesFitbit.length;  i++) {
+  console.log("Some number " + devicesFitbit[i] + ".");
+  }
+  var devicesFitbitFunction = function () {
+    console.log("There exist some number of HAH Model patient complications.");
+    };
+    devicesFitbitFunction();
+    var devicesFitbit = "patientCalories";
+    console.log("Some number of " + devicesFitbit);
+    if (devicesFitbit.length > 14) {
+      console.log("Obesity patients living with Type 2 Diabetes visit Fitbit website.");
+      }
+      var botSifidious;
+      botSifidious;
+      var botSifidious = ["Atoms", "Molecules", "AminoAcids", "Proteins", "Cells", "Tissues", "Organs"];
+      var Molecules;
+      Molecules;
+      var Molecules = ["Glucose", "Sugar", "Dextrose"];
+      var patientComplications = 0;
+      while (patientComplications < 13) {
+        console.log("Some number of Type 2 Diabetes patients may present random Glucose Levels greater than " + patientComplications + " millimoles per liter.");
+        patientComplications++;
+        }
+        console.log("Patient Complications Type 2 Diabetes random Glucose");
+      var devicesFitbit = 0;
+      while (devicesFitbit < 13) {
+        console.log("Some number of Type 2 Diabetes patients may sleep " + devicesFitbit + " hours.");
+        devicesFitbit++;
+        }
+        console.log("Fitbit patient sleep");
+        var devicesFitbit = "patientCalories";
+        console.log("Breakfast may increase " + devicesFitbit);
+        if (devicesFitbit.length > 14) {
+          console.log("Patient must check Dexcom for random Glucose levels.");
+          } 
+          var Dexcom;
+          Dexcom;
+            var Dexcom = 0;
+            while (Dexcom < 3) {
+              console.log("Some number of individuals living with Type 2 Diabetes must check random Glucose levels every " + Dexcom + " hours.");
+              Dexcom++;
+              }
+              console.log("Dexcom random Glucose levels");
+              var devicesFitbit = "patientWalkingSpeed";
+              console.log("Some number " + devicesFitbit);
+              if (devicesFitbit.length > 18) {
+                console.log("Random Glucose Levels occur with patientComplications.");
+                }
+              var devicesFitbit = "patientWalkingSpeed";
+              console.log("Some number StepsPerMinute is " + devicesFitbit);
+              if (devicesFitbit.length > 18) {
+                console.log("Patient random Glucose level is between 0.001 milligrams per deciliter and 99.999 milligrams per deciliter.");
+                }
+                var patientNoComplications = "obesity";
+                console.log("Consuming alcohol may cause " + patientNoComplications);
+                if (patientNoComplications.length > 6) {
+                  console.log("Decrease your alcohol consumption by 8 ounces per day.");
+                  }
+                  var patientNoComplications = "hypertension";
+                  console.log("Consuming alcohol also may lead to " + patientNoComplications);
+                  if (patientNoComplications.length > 11) {
+                    console.log("Decrease your alcohol consumption by 16 ounces per day to prevent onset of prediabetes.");
+                    }
+                    var RandomGlucoseLevel;
+                    RandomGlucoseLevel;
+                    var RandomGlucoseLevel = ["Fitbit", "Dexcom", "patientNoComplications", "patientComplications"];
+                    var botCAOSMedicalLab;
+                    botCAOSMedicalLab;
+                    
